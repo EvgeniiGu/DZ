265e3d5f3cc05bdb658ed64b56d470a56c3e1be5
EvgeniiGu <gyshin.evgenii@yandex.ru>
2021-03-20 15:10:01 +0300

    Updated

--- Lab_1_2/Lab_1_2.cpp
+++ Lab_1_2/Lab_1_2.cpp
@@ -1,5 +1,16 @@
 ﻿#include <iostream>
 using namespace std;
+/*
+Структура для класса Complex
+*/
+struct StrComplex
+{
+	float re = 0,
+		  im = 0;
+};
+/*
+Структура для класса Complex
+*/
 class Complex
 {
 public:
@@ -13,76 +24,23 @@ public:
 	void Set(float a, float b);
 	float GetRealPart();
 	float GetImaginaryPart();
+	StrComplex ComplexNum;
 private:
 	const float eps = 0.00001;
-	float x1 = 0;
-	float y1 = 0;
 };
 
-Complex::Complex(float a, float b)
+/*
+Структура для класса Vector3D
+*/
+struct StrСoordinatesVector
 {
-	x1 = a;
-	y1 = b;
-}
-
-void Complex::Add(float c, float d)
-{
-	x1 += c;
-	y1 += d;
-}
-
-void Complex::Sub(float c, float d)
-{
-	x1 -= c;
-	y1 -= d;
-}
-
-void Complex::Mul(float c, float d)
-{
-	x1 *= c;
-	y1 *= d;
-}
-
-void Complex::Div(float c, float d)
-{
-	if (c != 0 and d != 0 )
-	{
-	x1 /= c;
-	y1 /= d;
-	}
-	else
-	{
-		cout << "Division error\n";
-	}
-}
-
-bool Complex::Equ(float c, float d)
-{
-	if (fabs(x1 - c) < eps and fabs(y1 - d) < eps) return 1;
-	else return 0;
-}
-
-void Complex::Conj()
-{
-	y1 = -y1;
-}
-
-void Complex::Set(float a, float b)
-{
-	x1 = a;
-	y1 = b;
-}
-
-float Complex::GetRealPart()
-{
-	return x1;
-}
-
-float Complex::GetImaginaryPart()
-{
-	return y1;
-}
-
+	int x,
+		y,
+		z;
+};
+/*
+Структура для класса Vector3D
+*/
 class Vector3D
 {
 public:
@@ -94,72 +52,88 @@ public:
 	bool VectorComp (int x, int y, int z);
 	float CalcLength ();
 	bool LengthComp (int x, int y, int z);
+	StrСoordinatesVector Coordinates;
 private:
-	int x1,
-	    y1,
-	    z1;
 	float LengthOfVector();
 };
-
-Vector3D::Vector3D(int x, int y, int z)
+/*
+Структуры для класса ModelWindow
+*/
+struct StrSizeScreen
 {
-	x1 = x;
-	y1 = y;
-	z1 = z;
-
-}
-
-void Vector3D::Add(int x, int y, int z)
+	int x = 0,
+		y = 0;
+};
+struct StrCoordinates//UpLeftCorner
 {
-	x1 += x;
-	y1 += y;
-	z1 += z;
-}
-
-void Vector3D::Sub(int x, int y, int z)
+	int x = 0,
+		y = 0;
+};
+struct StrSize
 {
-	x1 -= x;
-	y1 -= y;
-	z1 -= z;
-}
-
-int Vector3D::ScalarMul(int x, int y, int z)
+	int horizontal = 0,
+		vertical = 0;
+};
+struct StrWindowColor
 {
-	return x1 * x + y1 * y + z1 * z;
-}
-
-void Vector3D::MulNum(int t)
+	uint8_t R = 0,
+		G = 0,
+		B = 0;
+};
+/*
+Структуры для класса ModelWindow
+*/
+class ModelWindow
 {
-	x1 += t;
-	y1 += t;
-	z1 += t;
-}
+public:
+	ModelWindow(int x, 
+				int y, int sizeHorizontal, 
+					   int sizeVertical, uint8_t r, 
+										 uint8_t g, 
+										 uint8_t b, bool state, bool frameState);
+	ModelWindow(int x,
+				int y, int sizeHorizontal,
+					   int sizeVertical, uint8_t r,
+										 uint8_t g,
+										 uint8_t b, bool state);
+	ModelWindow(int x,
+				int y, int sizeHorizontal,
+					   int sizeVertical, uint8_t r,
+										 uint8_t g,
+										 uint8_t b);
+	ModelWindow(int x,
+				int y, int sizeHorizontal,
+					   int sizeVertical);
+	ModelWindow(int x,
+				int y);
+	ModelWindow();
+	~ModelWindow();
+	void getSizeScreen(int sizeX, int sizeY);
+	void Move(int x, int y);
+	void ChangeSize(int x, int y);
+	void ChangeColor(int R, int G, int B);
+	void ChangeState(bool state);
+	void ChangeFrameCondition(bool state);
+	bool getStatus();
+	bool getStatusFrame();
+	//void moveHorizontal();
+	//void moveVertical();
+	StrSizeScreen SizeScreen;
+	StrCoordinates Coordinates;
+	StrWindowColor WindowColor;
+	StrSize Size;
+private:
+	string WindowTitle;
+	bool State = 0;
+	bool FrameCondition = 0;
+};
 
-bool Vector3D::VectorComp(int x, int y, int z)
-{
-	if (x1 == x && y1 == y && z1 == z) return 1;
-	else return 0;
-}
 
-float Vector3D::CalcLength()
-{
-	return LengthOfVector();
-}
 
-bool Vector3D::LengthComp(int x, int y, int z)
-{
-	if (LengthOfVector() == sqrt(x ^ 2 + y ^ 2 + z ^ 2)) return 1;
-	else return 0;
-}
-
-float Vector3D::LengthOfVector()
-{
-	return sqrt(x1 ^ 2 + y1 ^ 2 + z1 ^ 2);
-}
 
 
 
 int main()
 {
-    cout << "work in progress...\n";
+	cout << "work in progress...\n";
 }
